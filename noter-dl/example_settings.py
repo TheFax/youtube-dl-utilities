379ffca8fb789e98a4b321f24292132c52f97a81
TheFax <fabioslurp@yahoo.it>
2020-04-05 15:56:59 +0200

    Modified settings file

--- noter-dl/example_settings.py
+++ noter-dl/example_settings.py
@@ -1,3 +1,5 @@
+#Rename this file to: settings.py and edit it.
+
 config = dict(
 
     JSON_LINK = "https://xxxx/index.php?list=1",
@@ -10,14 +12,14 @@ config = dict(
                           '--audio-format','mp3', \
                           '--audio-quality','0',\
                           '-k',\
-                          '-o','./output/%(title)s-%(id)s.%(ext)s'],
+                          '-o','./output/%(title)s - %(id)s.%(ext)s'],
 
     COMMAND_BEST_VIDEO = ['youtube-dl',\
                           '-f','bestvideo+bestaudio',\
-                          '-o','./output/%(title)s-%(id)s.%(ext)s'],
+                          '-o','./output/%(title)s - %(id)s.%(ext)s'],
 
     COMMAND_STANDARD   = ['youtube-dl',\
                           '-f','best',\
-                          '-o','./output/%(title)s-%(id)s.%(ext)s'],
+                          '-o','./output/%(title)s - %(id)s.%(ext)s'],
 
 )
