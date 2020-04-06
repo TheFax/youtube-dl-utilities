c41d523060cddf5ae9249af029f14abb35df1bb2
TheFax <fabioslurp@yahoo.it>
2020-04-06 09:53:56 +0200

    Debugged version

--- noter-dl/noter-dl.py
+++ noter-dl/noter-dl.py
@@ -9,39 +9,49 @@ GOOD=0
 FAIL=-1
 
 def main():
+    #Richiedo al server la lista
     r = requests.get(web_settings.web_config['JSON_LINK'])
     if r.status_code != 200:
         #r.status_code conterrà 200 se la richiesta è andata a buon fine
         print("[ERR] impossibile contattare il server contenente la lista json")
         exit(-1)
 
+    #Converto la risposta json in un array multidimensionale
     values=r.json()
     #print(values)
 
+    #Analizzo riga per riga tutto l'array
     for x in range(len(values)):
         print("\n")
+
         #print("ID {} - Analisi della nota".format(values[x]['ID']))
+
+        #Audio
         if values[x]['title'] == "a":
             print("ID {} - Sarà scaricato l'audio alla massima qualità".format(values[x]['ID']))
             status = download(values[x]['content'], settings.config['COMMAND_BEST_AUDIO'])
             feedbackNote(values[x]['ID'], status)
-
+        
+        #Video
         elif values[x]['title'] == "v":
             print("ID {} - Audio e video saranno scaricati e muxati insieme alla massima qualità".format(values[x]['ID']))
             status = download(values[x]['content'], settings.config['COMMAND_BEST_VIDEO'])
             feedbackNote(values[x]['ID'], status)
 
+        #Both
         elif values[x]['title'] == "b":
             print("ID {} - Sarà scaricato l'audio (massima qualità) e anche il video (massima qualità)".format(values[x]['ID']))
             status = download(values[x]['content'], settings.config['COMMAND_BEST_AUDIO'])
             status = status + download(values[x]['content'], settings.config['COMMAND_BEST_VIDEO'])
             feedbackNote(values[x]['ID'], status)
 
+        #Normal
         elif values[x]['title'] == "n":
             print("ID {} - Sarà scaricato il video in formato standard".format(values[x]['ID']))
             status = download(values[x]['content'], settings.config['COMMAND_STANDARD'])
             feedbackNote(values[x]['ID'], status)
         
+        #Lista
         elif values[x]['title'] == "l":
             print("ID {} - Sarà scaricata l'intera playlist".format(values[x]['ID']))
             status = download(values[x]['content'], settings.config['COMMAND_PLAYLIST'])
@@ -50,19 +60,30 @@ def main():
         else:
             print("ID {} - La nota sarà ignorata poichè non sembra diretta a questo script".format(values[x]['ID']))
 
+    #Fine dello script senza errori
     exit(0)
 
 def feedbackNote(identifier, status):
+    #Questa funzione raccoglie il feedback e decide cosa fare
+
+    #Se è andato tutto bene, elimino la nota
     if status == GOOD:
+        print("[INFO] Elimino la nota ID {}".format(identifier))
         deleteNote(identifier)
 
 def deleteNote(identifier):
+    #Questa funzione elimina la nota con ID dato come argomento
     link = web_settings.web_config['DEL_LINK'] + str(identifier)
     r = requests.get(link)
 
 def download(youtubeLink, command):
+    #Questa funzione si occupa di scaricare materialmente il video YouTube
+    #Argomento 1: youtubeLink - Il link da scaricare.
+    #Argomento 2: command - è una LIST contenente il nome del programma da richiamare e tutti gli argomenti da passargli
+
     #print (youtubeLink)
     #print (command)
+    #print (type(command))
 
     #if len(youtubeLink) == 11:
     #    print("[INFO] Il link fornito verrà normalizzato col prefisso YouTube.")
@@ -73,12 +94,11 @@ def download(youtubeLink, command):
 
     print("[INFO] Download del link: {}".format(youtubeLink))
     cmd = command.copy()
-    cmd.append(chr(34))
     cmd.append(youtubeLink)
-    cmd.append(chr(34))
 
     try:
         #print (cmd)
+        #res = subprocess.Popen(cmd, stderr=subprocess.STDOUT)
         res = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     except OSError:
         #raise #PER DEBUG
@@ -87,19 +107,27 @@ def download(youtubeLink, command):
 
     res.wait()
 
-    output = res.stdout.read()
-    #In questo punto del programma, output è una lista e contiene riga per riga
-    #  tutte le info ritornate da youtube-dl
-    #TODO: analizzare il ritorno di youtube-dl alla ricerca di errori o info utili
+    output_out = res.stdout.read()
+    output_err = res.stderr.read()
+    return_code = res.returncode
+    
+    #In questo punto del programma, output_xxx sono dei b'' bytes e contengono riga per riga
+    # tutte le info ritornate da youtube-dl. Ogni riga è delimitata da un carattere di escape '/n'
+        
+    #print("VALORE DI OUTPUT:")
+    #print(output_out)   # returns b''
+    #print(output_err)   # returns b''
 
-    #print(output)
+    if output_err.find(b'ERROR') != -1 :
+        print("[ERR] Impossibile scaricare {} - ERROR in the output pipe".format(youtubeLink))
+        return FAIL
 
-    if res.returncode !=0:
+    if return_code !=0 :
         print("[ERR] Impossibile scaricare {} - Returncode: {}".format(youtubeLink,res.returncode))
         return FAIL
-    else:
-        print("[INFO] {} scaricato con successo.".format(youtubeLink))
-        return GOOD
+
+    print("[INFO] {} scaricato con successo.".format(youtubeLink))
+    return GOOD
 
 if __name__ == '__main__':
    main()
--- noter-dl/settings.py
+++ noter-dl/settings.py
@@ -1,24 +1,28 @@
 config = dict(
     COMMAND_BEST_AUDIO = ['youtube-dl', \
-                          '-f','bestaudio', \
-                          '--extract-audio', \
-                          '--audio-format','mp3', \
-                          '--audio-quality','0',\
-                          '-k',\
-                          '-o','./output/%(title)s - %(id)s.%(ext)s'],
+                            '--socket-timeout','15', \
+                            '-f','bestaudio', \
+                            '--extract-audio', \
+                            '--audio-format','mp3', \
+                            '--audio-quality','0',\
+                            '-k',\
+                            '-o','./output/%(title)s - %(id)s.%(ext)s'],
 
     COMMAND_BEST_VIDEO = ['youtube-dl',\
-                          '-f','bestvideo+bestaudio',\
-                          '-o','./output/%(title)s - %(id)s.%(ext)s'],
+                            '--socket-timeout','15', \
+                            '-f','bestvideo+bestaudio',\
+                            '-o','./output/%(title)s - %(id)s.%(ext)s'],
 
     COMMAND_STANDARD   = ['youtube-dl',\
-                          '-f','best',\
-                          '-o','./output/%(title)s - %(id)s.%(ext)s'],
+                            '--socket-timeout','15', \
+                            '-f','best',\
+                            '-o','./output/%(title)s - %(id)s.%(ext)s'],
 
     COMMAND_PLAYLIST   = ['youtube-dl',\
-                          '-f','best',\
-                          '-i',\
-                          '--yes-playlist',\
-                          '-o','./output/%(title)s - %(id)s.%(ext)s'],
+                            '--socket-timeout','15', \
+                            '-f','best',\
+                            '-i',\
+                            '--yes-playlist',\
+                            '-o','./output/%(title)s - %(id)s.%(ext)s'],
 
 )
