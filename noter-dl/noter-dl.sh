92c855c7b5e469feb28fde75f0ef7dadf149b186
TheFax <19174767+TheFax@users.noreply.github.com>
2020-06-11 08:10:02 +0200

    Aggiornamento youtube-dl

--- noter-dl/noter-dl.sh
+++ noter-dl/noter-dl.sh
@@ -7,6 +7,9 @@ cd "${0%/*}"
 wget -q https://github.com/TheFax/youtube-dl-utilities/raw/master/noter-dl/noter-dl.py -O noter-dl.py
 wget -q https://github.com/TheFax/youtube-dl-utilities/raw/master/noter-dl/settings.py -O settings.py
 
+#Aggiorno youtube-dl
+youtube-dl -U
+
 #Rendo eseguibile lo script
 chmod a+rx ./noter-dl.py
 
