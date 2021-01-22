88dacd432f95248e27d9c72a3dae4298c719aac2
TheFax <19174767+TheFax@users.noreply.github.com>
2021-01-22 22:19:24 +0100

    Removed AVI extension because it was problematic

--- video_renamer/renamer.sh
+++ video_renamer/renamer.sh
@@ -2,7 +2,7 @@
 
 shopt -s nullglob
 
-for f in *.{mkv,mp4,webm,avi}
+for f in *.{mkv,mp4,webm}
 do
   size=$(ffprobe -v error -select_streams v:0 -show_entries stream=width,height -of csv=s=x:p=0 "$f")
   #echo "($size)|$f|"
