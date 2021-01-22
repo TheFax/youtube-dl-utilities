74a09ed38b6457acac5a4c08a3357fb6789c00e6
TheFax <19174767+TheFax@users.noreply.github.com>
2021-01-22 21:43:25 +0100

    Update renamer.sh

--- video_renamer/renamer.sh
+++ video_renamer/renamer.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-for f in *.mkv
+shopt -s nullglob
+
+for f in *.{mkv,mp4,webm,avi}
 do
   height=$(ffprobe -v error -show_entries stream=height -of csv=p=0 "$f")
   if [ "$height" -eq 2160 ] || [ "$height" -eq 1080 ] || [ "$height" -eq 720 ]; then
-    mv "$f" "${f%.*} ($height).${f##*.}"
+
   fi
+  mv "$f" "${f%.*} ($height).${f##*.}"
 done
